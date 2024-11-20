dcf8b0e186574078b55034ffb17fb4a86abe78e5
sebastia <cucho@bash>
2024-11-20 10:26:56 -0500

    The icons in the sidebar and a button in the header to log out were changed

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/api/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -11,8 +13,16 @@ import { RouterModule } from '@angular/router';
 })
 export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<void>();
+
+  constructor(private authService: AuthService, private router: Router) {}
+
   onToggleSidebar() {
     this.toggleSidebar.emit();
+  }
+  
+  onLogout(): void {
+    this.authService.logout(); // Elimina el token de autenticación
+    this.router.navigate(['/login']); // Redirige al usuario a la página de inicio de sesión
   }
 
 }
