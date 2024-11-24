6557e328013ba8b971a05327edb1efd4aabce5d5
jdoterot <jdoterot@atlas.com.co>
2024-11-24 17:37:57 -0500

    enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1' ,
-  apiAuthUrl: 'http://localhost:8080/api/v1' 
+  apiUrl: 'https://gopr7g37j3.execute-api.us-east-1.amazonaws.com/prod/api/v1' ,
+  apiAuthUrl: 'https://gopr7g37j3.execute-api.us-east-1.amazonaws.com/prod/api/v1' 
 };
