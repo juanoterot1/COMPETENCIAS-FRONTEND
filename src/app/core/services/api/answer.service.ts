e525f4431cfad55b60bc637cbdfd29c6cca6ff33
jdoterot <jdoterot@atlas.com.co>
2024-11-24 18:55:04 -0500

    change answer service

--- src/app/core/services/api/answer.service.ts
+++ src/app/core/services/api/answer.service.ts
@@ -1,10 +1,10 @@
-// src/app/core/services/answer.service.ts
+// src/app/core/services/api/answer.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../../models/api-response.model';
 import { Answer } from '../../models/answer.model';
-import { environment } from '../../../../environment/environment';
+import { environment } from '../../../../environment/environment'; // Asegúrate de que la ruta sea correcta
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,14 @@ export class AnswerService {
 
   constructor(private http: HttpClient) {}
 
-  createAnswer(answer: Partial<Answer>, idUser: number): Observable<ApiResponse<Answer>> {
-    return this.http.post<ApiResponse<Answer>>(this.apiUrl, {
-      ...answer,
-      id_user: idUser
-    });
+  // Método para crear una respuesta individual
+  createAnswer(answer: Partial<Answer>): Observable<ApiResponse<Answer>> {
+    return this.http.post<ApiResponse<Answer>>(this.apiUrl, [answer]);
+  }
+
+  // Método para crear respuestas en batch
+  createAnswers(answers: Partial<Answer>[]): Observable<ApiResponse<Answer[]>> {
+    return this.http.post<ApiResponse<Answer[]>>(this.apiUrl, answers);
   }
 
   getAnswerById(id: number): Observable<ApiResponse<Answer>> {
@@ -34,26 +37,19 @@ export class AnswerService {
     let params = new HttpParams()
       .set('page', page.toString())
       .set('per_page', perPage.toString());
-  
+
     if (idEvaluation) {
       params = params.set('id_evaluation', idEvaluation.toString());
     }
     if (idQuestion) {
       params = params.set('id_question', idQuestion.toString());
     }
-  
+
     return this.http.get<ApiResponse<Answer[]>>(`${this.apiUrl}`, { params });
   }
-  
-  
-  
-  
 
-  updateAnswer(id: number, answer: Partial<Answer>, idUser: number): Observable<ApiResponse<Answer>> {
-    return this.http.put<ApiResponse<Answer>>(`${this.apiUrl}/${id}`, {
-      ...answer,
-      id_user: idUser
-    });
+  updateAnswer(id: number, answer: Partial<Answer>): Observable<ApiResponse<Answer>> {
+    return this.http.put<ApiResponse<Answer>>(`${this.apiUrl}/${id}`, answer);
   }
 
   deleteAnswer(id: number): Observable<ApiResponse<void>> {
